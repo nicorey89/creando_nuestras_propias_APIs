e763ab4c670362e580196c89243935f10a3af83c
nicorey89 <nicolasthian8907@gmail.com>
2023-05-01 23:06:24 -0300

    subiendo TP

---
+++ src/routes/api/genresRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const genresController = require('../../controllers/api/genresController');
+
+router.get('/genres', genresController.list);
+router.get('/genres/:id', genresController.detail);
+
+
+module.exports = router;
